f6848d9c9ce4867c0a16ecb804d77cb0b59b6a88
Adrian Smith <agnomen@gmail.com>
2016-06-14 18:41:39 -0400

    Set up view in C3D.

--- src/c3d.js
+++ src/c3d.js
@@ -1,11 +1,25 @@
 var C3D = (function () {
     "use strict";
     
+    function View() {
+        this.clearColor = [0, 0, 0, 1];
+        this.maximize = true;
+        this.updateInDraw = false;
+        this.updateInterval = 16;
+    }
+    
+    View.prototype.update = function (now, elapsed, keyboard, pointer) {
+        // Should put something here.
+    };
+    
+    View.prototype.render = function (room, width, height) {
+        room.clear(this.clearColor);
+        room.drawTest();
+    };
+    
     window.onload = function(e) {
         MAIN.runTestSuites();
-        var test3D = new MAIN.Test3D();
-        test3D.maximize = true;
-        MAIN.start(document.getElementById("canvas3D"), test3D);
+        MAIN.start(document.getElementById("canvas3D"), new View());
     };
     
     return {
