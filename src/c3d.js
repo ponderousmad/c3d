1889ec57feb69b16be28395c4354765d257fd25f
Adrian Smith <agnomen@gmail.com>
2016-06-14 22:38:34 -0400

    Set up drop image on to canvas support.

--- src/c3d.js
+++ src/c3d.js
@@ -17,9 +17,38 @@ var C3D = (function () {
         room.drawTest();
     };
     
+    View.prototype.loadImage = function (event) {
+        var image = new Image();
+        image.src = event.target.result;
+    };
+    
     window.onload = function(e) {
         MAIN.runTestSuites();
-        MAIN.start(document.getElementById("canvas3D"), new View());
+        var canvas = document.getElementById("canvas3D"),
+            view = new View();
+        MAIN.start(canvas, view);
+
+        // Show the copy icon when dragging over. Seems to only work for chrome.
+        canvas.addEventListener('dragover', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            e.dataTransfer.dropEffect = 'copy';
+        });
+
+        // Get file data on drop
+        canvas.addEventListener('drop', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            var files = e.dataTransfer.files;
+            if (files.length == 1) {
+                var file = files[0];
+                if (file.type.match(/image.*/)) {
+                    var reader = new FileReader();
+                    reader.onload = function(loadEvent) { view.loadImage(loadEvent); };
+                    reader.readAsDataURL(file); // start reading the file data.
+                }
+            }
+        });
     };
     
     return {
